462010f1c360f6019235178bbc798c229bbda3bc
Svetla Todorova <svetlahristova79@gmail.com>
2025-01-21 00:12:28 +0200

    Create main.hbs in Layouts
    Render Home page

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.set('views', './src/views');
 app.use('/static', express.static('src/static')) 
 
 app.get('/', (req, res) => {
-    res.render('home', {layout: false});
+    res.render('home');
 });
 
 app.listen(5000, ()=> console.log('Server is listening of http://localhost:5000...'))
