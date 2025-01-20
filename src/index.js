676bfc0073118bd41d8c23f8c3da073ccc6a4097
Svetla Todorova <svetlahristova79@gmail.com>
2025-01-20 14:11:02 +0200

    Install Express
    The server is listening and connected

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app=express();
+
+app.get('/', (req, res) => {
+    res.send('It Works');
+});
+
+app.listen(5000, ()=> console.log('Server is listening of http://localhost:5000...'))
