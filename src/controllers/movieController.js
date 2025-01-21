6ad91704014b81eb69d7869d33e08df195f5c265
Svetla Todorova <svetlahristova79@gmail.com>
2025-01-21 14:15:45 +0200

    Set up static Details page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -3,7 +3,12 @@ import { Router } from "express";
 const movieController=Router()
 
 movieController.get('/create', (req, res) => {
-    res.render('create')
-})
+    res.render('create');
+});
+
+movieController.get('/:movieId/details', (req, res) => {
+    res.render('details');
+;
+});
 
 export default movieController
