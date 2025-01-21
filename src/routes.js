6f145ecd0da0f0cf8623617609e8b421dcfbcc1f
Svetla Todorova <svetlahristova79@gmail.com>
2025-01-21 13:51:59 +0200

    Add Create page

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import homeController from './controllers/homeController.js'
+import homeController from './controllers/homeController.js';
+import movieController from "./controllers/movieController.js";
 
 const routes=Router()
 
 routes.use(homeController);
+routes.use(movieController);
 
 routes.get('*', (req, res)=>{
     res.render('404')
-})
-
+});
 
 export default routes
