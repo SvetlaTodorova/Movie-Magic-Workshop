028d41548991f213b577a92c1378caec84720c02
Svetla Todorova <svetlahristova79@gmail.com>
2025-01-21 12:47:24 +0200

    Create routes to hold all the controllers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import handlebars from 'express-handlebars'
-import homeController from './controllers/homeController.js';
+import routes from './routes.js';
 
 const app=express();
 
@@ -13,11 +13,6 @@ app.set('views', './src/views');
 
 app.use('/static', express.static('src/static')) 
 
-app.use(homeController)
-
-
-app.get('*', (req, res)=>{
-    res.render('404')
-})
+app.use(routes)
 
 app.listen(5000, ()=> console.log('Server is listening of http://localhost:5000...'))
