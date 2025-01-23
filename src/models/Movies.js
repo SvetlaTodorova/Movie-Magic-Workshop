7fa6febee00f13700237af69cdb804240dbc98c4
Svetla Todorova <svetlahristova79@gmail.com>
2025-01-23 14:13:35 +0200

    Create Movie model

---
+++ src/models/Movies.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const movieSchema = new Schema({
+      
+        title: String,
+        categor: String,
+        genre: String,
+        director: String,
+        year: Number,
+        imageURL: String,
+        rating: Number,
+        description: String,
+
+});
+
+const Movie=model('Movie', movieSchema);
+
+export default Movie;
+
