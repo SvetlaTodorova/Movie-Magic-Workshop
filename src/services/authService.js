c6dbb215229588facc094ba3237349728143e47d
Svetla Todorova <svetlahristova79@gmail.com>
2025-02-02 00:36:22 +0200

    Save hash password at DB

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,12 @@
 
-import User from "../models/User.js"
+import User from "../models/User.js"; 
+import bcrypt from 'bcrypt'
 export default {
-    register(userData) {
-        return User.create(userData)
+    async register(userData) {
+        
+        const salt = await bcrypt.genSalt(10)
+        userData.password =  await bcrypt.hash(userData.password, salt)
+        
+        return User.create(userData);
     }
 }
